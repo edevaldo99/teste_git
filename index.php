5006930b3089a2aef5f558a2083c95cb431eeab7
Edevaldo Mazzucco <edevaldomazzucco1999@gmail.com>
2017-01-03 16:39:44 -0200

    primeira versao

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>Teste de git</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+
+  <body>
+    <div id="wrap">
+      <h1>Olá! Seja bem vindo ao meu index.php. Fique a vontade por aqui...</h1>
+      <p>Esse é um teste de git.</p>
+      <p>Layout feio né? kkk</p>
+    </div>
+  </body>
+</html>
